0a41d940ae658cc4225f3543448c49f5742e2502
Brett Longworth <blongworth@whoi.edu>
2022-01-09 11:52:27 -0500

    find nearest standards by time

--- R/nearest_standards.R
+++ R/nearest_standards.R
@@ -1,3 +1,29 @@
+
+# TODO: How does snicser handle ties?
+# I think the bubble sort in SNICSer's FindNearestStandards will always pick the
+# first element in a tie, which is the same as these functions
+
+
+#' Find nearest standards in time
+#'
+#' @param types A character vector of sample type ids where "S" denotes a normalizing standard
+#' @param times A vector of run timestamps
+#' @param run The index of the run to find standards for
+#' @param n number of standard runs
+#' @param self Allow self normalization
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nearest_standards_time <- function(types, times, run, n, self = FALSE) {
+  stopifnot(length(types) == length(times))
+  drop <- if (self) NULL else run
+  stds <- find_standard_runs(types, drop = drop)
+  nearest <- nearest_standards(times[run], times[stds], n)
+  which(times %in% nearest)
+}
+
 #' Find nearest standards
 #'
 #' @param run run index
@@ -17,8 +43,8 @@ nearest_standards <- function(run, stds, n) {
 #' Create an index of standard runs
 #'
 #'
-#' @param type A character vector of sample type ids where "S" denotes a normalizing standard
-#' @param run The index of a runs to ignore (to avoid self-normalization)
+#' @param types A character vector of sample type ids where "S" denotes a normalizing standard
+#' @param drop The index of a runs to ignore (to avoid self-normalization)
 #'
 #' @return The indexes of standard runs
 #' @export
--- tests/testthat/test-nearest_standards.R
+++ tests/testthat/test-nearest_standards.R
@@ -7,12 +7,21 @@ test_that("nearest_standards finds nearest standards by run number", {
   expect_equal(nearest_standards(5, c(3,4,10,20), 2), c(4,3))
 })
 
+test_that("nearest_standards finds nearest standards by time", {
+  times <- seq(as.POSIXct("2022-01-01"), by = "min", length.out = 10)
+  expect_equal(nearest_standards(times[5], times, 1), times[5])
+  expect_equal(nearest_standards(times[5], times, 5), times[c(5,4,6,3,7)])
+})
+
 test_that("nearest_standards Fails with bad inputs", {
   expect_error(nearest_standards(0, c(1,3), 2))
   expect_error(nearest_standards(1, 0, 2))
   expect_error(nearest_standards(1, c(0,3), 2))
   expect_error(nearest_standards(1, c(1,3), 0))
   expect_error(nearest_standards(1, integer(0), 0))
+})
+
+test_that("nearest_standards finds same standards as SNICSer", {
 })
 
 test_that("find_standard_runs finds standard runs", {
@@ -27,4 +36,8 @@ test_that("find_standard_runs handles no standard runs", {
 test_that("find_standard_runs drops runs", {
   expect_equal(find_standard_runs(c("B", "S", "SS", "S"), 2), c(4))
   expect_equal(find_standard_runs(c("B", "S", "SS", "S"), c(2, 4)), integer(0))
+})
+
+
+test_that("nearest_standards_time returns same set as SNICSer", {
 })
